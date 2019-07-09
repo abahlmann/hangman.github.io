12ae032e516585a1362ad1762d46614c4e76b862
Abby Bahlmann <abbyb777@gmail.com>
2019-07-09 12:27:53 -0400

    2.2

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,81 @@
 
-   var wordlist=[
-    "oval",
-    "tentacle",
-    "bright",
-    "yard",
-    "hole",
-    "pollution",
-    "nail",
-    "fish",
-    "tractor",
-    "dinosaur",
-    "walrus",];
+var wordlist=[
+    "oval",//0
+    "tentacle",//1
+    "bright",//2
+    "yard",//3
+    "hole",//4
+    "pollution",//5
+    "nail",//6
+    "fish",//7
+    "tractor",//8
+    "dinosaur",//9
+    "walrus",];//10
 
-   //functions for wordlist:
+var guessedWord=[];
 
-var guess=[];
+var guess={
+        lose:false,
+        win:false,
+        correctGuess:false,
+        currentWord:" ",
+        guessedWord:" ",
+        lettersGuessed:0,
+        lettersNeeded:0,
 
+        newWord: function(){
+            //random number/index
+            var x=Math.floor(Math.random()*11);
+            this.currentWord=wordlist[x];
 
-    guess={
-        
+            //push guessedWord array
+            for (var i=0;i<this.currentWord.length;i++){
+                guessedWord.push("_");
+            }//close for-loop
+
+            //set lettersNeeded
+            this.lettersNeeded=this.currentWord.length;
+        },//close newWord
+
+        checkGuess: function(){
+            //reset guess
+            this.correctGuess=false;
+
+                //check each letter
+                for (var i=0;i<this.currentWord.length;i++){
+                    if (guess===this.currentWord.charAt(i)){
+                        guessedWord[i]=this.currentWord.charAt(i);
+                        this.correctGuess=true;
+                        this.lettersNeeded=this.lettersNeeded-1;
+                    }//close if
+                    else{
+                        this.lettersGuessed+=1;
+                        if (this.lettersGuessed<=15){
+                            lose=true;
+                        }//close if
+                    }//close else
+                }//close for-loop
+            
+             //check for win
+            if (this.lettersNeeded===0){
+                win=true;
+            }//close if
+
+        }//close checkGuess()
+    }//close guess
+document.onkeyup=function(start){
+    guess.newWord();
+    var guessBox = document.createElement("div");
+    for (var i=0;i<guess.currentWord.length;i++){
+    guessBox.innerHTML+=guessedWord[i]+" ";
     }
-    
-    var entryScreen = document.createElement("p");
-    entryScreen.onload = function() {
-        entryScreen.innerHTML="PRESS ANY KEY TO CONTINUE";
-        container.appendChild(entryScreen);
-    }
+    container.appendChild(guessBox);
+    do{
 
-    //the lines that become the words when you get them right
-    container.innerHTML=word.display();
+    document.onkeyup=function(letter){
+        guess=letter;
+        guess.checkGuess();
 
+    }//end letter/guess
+    } while(guess.win||!guess.lose);//end do-while
+}//end start
